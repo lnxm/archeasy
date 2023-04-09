677e26a12a76ad7886ca3acdb8e0a089763508b0
lnxm <103046706+lnxm@users.noreply.github.com>
2023-04-09 14:54:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,86 +1,81 @@
 #!/bin/bash
 
-echo "Welcome to Arch Linux Installation Script!"
+# Ensure system clock is accurate
+timedatectl set-ntp true
 
-# Partitioning the disk
-echo "Partitioning the disk..."
-parted --script /dev/sda \
-  mklabel gpt \
-  mkpart ESP fat32 1MiB 513MiB \
-  set 1 boot on \
-  mkpart primary ext4 513MiB 100%
+# Partition the disk with EFI and root partitions
+parted -s /dev/sda mklabel gpt mkpart primary fat32 1MiB 261MiB set 1 esp on mkpart primary ext4 261MiB 100%
 
-# Formatting the partitions
-echo "Formatting the partitions..."
-mkfs.fat -F32 /dev/sda1 >/dev/null
-mkfs.ext4 /dev/sda2 >/dev/null
+# Format the partitions
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
 
-# Mounting the partitions
-echo "Mounting the partitions..."
+# Mount the partitions
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-# Installing the base system
-echo "Installing the base system..."
-pacstrap /mnt base linux-lts linux-firmware intel-ucode >/dev/null
+# Set up the mirrorlist for fastest download speed
+pacman -Sy reflector
+reflector --verbose --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 
-# Generating fstab
-echo "Generating fstab..."
+# Install the base system and necessary packages
+pacstrap /mnt base base-devel linux-lowlatency linux-firmware intel-ucode grub efibootmgr networkmanager neofetch
+
+# Generate the fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Setting the timezone
-echo "Setting the timezone..."
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+# Copy the script to the new system for post-installation tasks
+cp $0 /mnt/root/post-install.sh
 
-# Generating the /etc/adjtime file
-echo "Generating the /etc/adjtime file..."
-arch-chroot /mnt hwclock --systohc
+# Chroot into the new system
+arch-chroot /mnt /bin/bash <<EOF
 
-# Setting the locale
-echo "Setting the locale..."
-echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
-arch-chroot /mnt locale-gen
+# Set the timezone and clock
+ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+hwclock --systohc
 
-# Setting the keyboard layout
-echo "Setting the keyboard layout..."
-echo "KEYMAP=cz-qwertz" > /mnt/etc/vconsole.conf
+# Set the language and keyboard layout
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+sed -i 's/#cs_CZ.UTF-8/cs_CZ.UTF-8/g' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=cz-qwertz' > /etc/vconsole.conf
 
-# Setting the hostname
-echo "Setting the hostname..."
-echo "arch-laptop" > /mnt/etc/hostname
+# Set the hostname and add the user
+echo 'e7440' > /etc/hostname
+echo '127.0.0.1  e7440.localdomain  e7440' >> /etc/hosts
+useradd -m -G wheel lnxm
+echo 'lnxm:1' | chpasswd
 
-# Adding matching entries to hosts
-echo "Adding matching entries to hosts..."
-echo "127.0.0.1	localhost" > /mnt/etc/hosts
-echo "::1		localhost" >> /mnt/etc/hosts
-echo "127.0.1.1	arch-laptop.localdomain	arch-laptop" >> /mnt/etc/hosts
+# Install and configure GRUB bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+grub-mkconfig -o /boot/grub/grub.cfg
 
-# Installing GRUB
-echo "Installing GRUB..."
-pacstrap /mnt grub efibootmgr dosfstools os-prober mtools >/dev/null
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB >/dev/null
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg >/dev/null
+# Install and configure KDE
+pacman -S plasma kde-applications-meta
+pacman -S konsole dolphin kate kwrite spectacle ksysguard yakuake kwalletmanager
 
-# Installing drivers for the Latitude E7440
-echo "Installing drivers for the Latitude E7440..."
-pacstrap /mnt xf86-input-synaptics xf86-video-intel >/dev/null
+# Install the Brave browser
+pacman -S brave
 
-# Installing KDE and necessary applications
-echo "Installing KDE and necessary applications..."
-pacstrap /mnt xorg plasma-desktop konsole dolphin kate pamac vlc brave discord-development >/dev/null
+# Install additional software
+pacman -S pamac vlc discord-devel
 
-# Enabling NetworkManager
-echo "Enabling NetworkManager..."
-arch-chroot /mnt systemctl enable NetworkManager
+# Install drivers for the Latitude E7440
+pacman -S xf86-input-synaptics xf86-video-intel
 
-# Configuring additional boot tweaks
-echo "Configuring additional boot tweaks..."
-echo "vm.swappiness=10" > /mnt/etc/sysctl.d/99-swappiness.conf
+# Install Intel QuickSync Video encoder
+pacman -S intel-media-driver libva-utils
 
-# Installing neofetch
-echo "Installing neofetch..."
-pacstrap /mnt neofetch >/dev/null
+# Enable NetworkManager service
+systemctl enable NetworkManager
 
-echo "Installation complete!"
+# Add neofetch to the terminal config
+echo 'neofetch' >> /etc/profile.d/neofetch.sh
+
+EOF
+
+# Reboot the system
+umount -R /mnt
+reboot
